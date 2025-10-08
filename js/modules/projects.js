2be8c257af16192dd28f85d24ebc982997236a0a
Neptali Calonge <36485332+nep-code@users.noreply.github.com>
2025-10-08 08:17:19 +0800

    Update projects.js

--- js/modules/projects.js
+++ js/modules/projects.js
@@ -51,7 +51,7 @@ export function init() {
 		}
 
 
-        gsap.utils.toArray(".project").forEach((el, i) => {
+        /*gsap.utils.toArray(".project").forEach((el, i) => {
             gsap.from(el, {
                 x: i % 2 === 0 ? 500 : -500,
                 opacity: 0,
@@ -63,7 +63,7 @@ export function init() {
                     toggleActions: "play none none reverse",
                 },
             });
-        });
+        });*/
 				
 
 				function AUTOSCALE() {
@@ -104,73 +104,3 @@ export function init() {
 					window.scrollTo(0, scrollY);
 				}
     }
-
-    function SKILLS() {
-        const carousel = document.getElementById("carousel");
-        if (!carousel) return;
-
-        const logo = gsap.utils.toArray(".Logo");
-        const radius = carousel.clientWidth / 1.5;
-        const proxy = document.createElement("div");
-        const wrap = gsap.utils.wrap(0, 1);
-        const drag = 3000;
-
-        let startProgress;
-
-        const spin = gsap.fromTo(
-            logo,
-            { rotationY: (i) => (i * 360) / logo.length },
-            {
-                rotationY: "-=360",
-                duration: 50,
-                ease: "none",
-                repeat: -1,
-                transformOrigin: `50% 50% ${-radius}px`,
-            }
-        );
-
-        Draggable.create(proxy, {
-            trigger: "#carousel",
-            type: "x",
-            inertia: true,
-            allowNativeTouchScrolling: false,
-            dragResistance: 0.1,
-            onPress() {
-                spin.timeScale(0);
-                startProgress = spin.progress();
-            },
-            onDrag: updateRotation,
-            onRelease() {
-                gsap.to(spin, { timeScale: 1, duration: 1 });
-            },
-            onThrowUpdate: updateRotation,
-            onThrowComplete() {
-                gsap.to(spin, { timeScale: 1, duration: 1 });
-            },
-        });
-
-        gsap.set("#carousel .Content", { perspective: 1000 });
-
-        function updateRotation() {
-            const p = startProgress + (this.startX - this.x) / drag;
-            spin.progress(wrap(p));
-        }
-
-        const logoState = new WeakMap();
-        gsap.ticker.add(() => {
-            logo.forEach((el) => {
-                const r = gsap.getProperty(el, "rotationY") % 360;
-                const n = (r + 360) % 360;
-                const isBack = n > 80 && n < 280;
-                if (logoState.get(el) !== isBack) {
-                    logoState.set(el, isBack);
-                    gsap.to(el, {
-                        duration: 0.2,
-                        opacity: isBack ? 0.5 : 1,
-                        filter: isBack ? "blur(4px)" : "blur(0px)",
-                        pointerEvents: isBack ? "none" : "auto",
-                    });
-                }
-            });
-        });
-}
